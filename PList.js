188938436334fb62e5358dc453dd6d6aaefb82a9
Danieljb5 <39220451+Danieljb5@users.noreply.github.com>
2021-06-29 21:13:55 +0100

    Update PList.js

--- PList.js
+++ PList.js
@@ -1,9 +1,10 @@
 let result;
 let error;
-let currentLine = 1;
+let currentLine = 15;
 let line = ' ';
 let file;
 let defaultURL = "";
+let currentTextSize = 15;
 
 function setup() {
   let params = getURLParams();
@@ -13,7 +14,7 @@ function setup() {
     file = 'main';
   }
   loadStrings(file, loaded, loadErr);
-  createCanvas(600, 600);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function loadErr(error)
@@ -43,15 +44,16 @@ function draw() {
 
 function parseLine()
 {
+  textSize(currentTextSize);
   if(match(line, '{i}') != null)
   {
     let arr = split(line, 'i}');
     let str;
     str = arr[1];
     imageMode(CORNER);
-    let imgL = currentLine - 1;
+    let imgL = currentLine - currentTextSize;
     drawImage(str, imgL);
-    currentLine += 10;
+    currentLine += 180;
   }
   else if(match(line, '{a}') != null)
   {
@@ -66,19 +68,32 @@ function parseLine()
       name = arr[1];
     }
     let a = createA(defaultURL + "?file=" + str, name);
-    a.position(10, (currentLine - 1) * 15);
-    currentLine += 1.3;
+    a.position(10, currentLine - currentTextSize);
+    currentLine += currentTextSize * 1.5;
+  }
+  else if(match(line, '{s}') != null)
+  {
+    let arr = split(line, 's}');
+    let str;
+    str = arr[1];
+    textSize(int(str));
+    currentTextSize = int(str);
   }
   else
   {
-    text(line, 10, currentLine * 15);
-    currentLine += 1.3;
+    let lines = [];
+    if(textWidth(line > windowWidth))
+    {
+      
+    }
+    text(line, 10, currentLine);
+    currentLine += currentTextSize * 1.5;
   }
 }
 
 function drawImage(str, imageLine)
 {
   loadImage(str, img => {
-    image(img, 10, imageLine * 15, 150, 150);
+    image(img, 10, imageLine, 150, 150);
   });
 }
